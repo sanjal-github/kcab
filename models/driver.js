3b85974c1044c2586cbdfcf8308265200257dade
Raj <kkusanjal@gmail.com>
2023-05-08 17:51:16 +0530

    creating driver model

---
+++ models/driver.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const driverSchema = new mongoose.Schema({
+    _id:
+    {
+        type:String,
+
+    },
+    name:
+    {
+        type:String,
+        required:true
+    },
+    address:
+    {
+        type:String,
+        required:true
+    },
+    mobile:
+    {
+        type:String,
+        required:true,
+    },
+    birth_date:
+    {
+        type:Date,
+        require:true
+    },
+    gender:
+    {
+        type:String,
+        required:true
+    },
+    photo:
+    {
+        type:String,
+        required:true
+    },
+    liscence_number:
+    {
+        type:String,
+        required:true,
+        unique:true
+    },
+    working:
+    {
+        type:Boolean,
+        default:true
+    }
+},
+{
+    timestamps:true
+})
+
+const driver = new mongoose.model("driver",driverSchema);
+module.exports = driver ;
