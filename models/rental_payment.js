a6ecf28b350629f077f4cbeefec66a8891c092ca
Raj <kkusanjal@gmail.com>
2023-05-22 15:03:09 +0530

    created rental payment models

---
+++ models/rental_payment.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+  _id:
+  {
+    type:Sṭring,
+  },
+  transaction_id:
+  {
+    type:String,
+  },
+  date:
+  {
+    type:Date
+  },
+  time:
+  {
+    type:Date
+  },
+  total_time:
+  {
+    type:Number
+  },
+  tax:
+  {
+    type:Number
+  },
+  total_Amount:
+  {
+    type:Number,
+  },
+  pay_mode:
+  {
+    type:String,
+    enum:["cash","online"]
+  },
+  status:
+  {
+    type:String
+  }
+
+})
